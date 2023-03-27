65d3e99b9159c35c9a1f8dd55d4d328b5672ab43
Benjhg <benjamin.herrera.garrido@alumnos.sip.cl>
2023-03-27 15:19:42 -0300

    ....

--- fundamentals/oop/fdasdas.py
+++ fundamentals/oop/fdasdas.py
@@ -9,13 +9,29 @@ class Usuario:
         else:
             self.balance -= amount
             print("Retiro exitoso. Saldo restante:", self.balance)
- 
+
+    def mostrar_balance(self):
+        print("tu saldo es", self.balance)
+
     def hacer_deposito(self, amount):
         self.balance += amount
-        print("Depósito exitoso. Saldo actual:", self.balance)                    
+        print("Depósito exitoso. Saldo actual:", self.balance)     
+
+    def tranferencia(self, amount,user):
+        if amount > self.balance:
+            print("Error: no hay suficiente saldo en la cuenta")
+        else:
+            self.balance -= self.balance
+            user.balance += self.balance
+            print("tu deposito fue exitoso y tu saldo quedo en", )
+            print
+
 
 juan = Usuario("Juan", 0)
 benjamin = Usuario("benjamin", 999999)
 
 juan.hacer_deposito(10)
-juan.hacer_retiro(500)
+juan.hacer_retiro(0)
+juan.hacer_deposito(100)
+juan.mostrar_balance()
+juan.tranferencia(110,benjamin)
