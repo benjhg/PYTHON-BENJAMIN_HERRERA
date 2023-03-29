069d64cab41d3ea0902abadf633ed7378ae44527
Benjhg <benjamin.herrera.garrido@alumnos.sip.cl>
2023-03-29 13:50:27 -0300

    Cuentas bancarias

---
+++ fundamentals/oop/Cuentabancaria.py
@@ -0,0 +1,13 @@
+class CuentaBancaria:
+    # ¡No olvides agregar algunos valores predeterminados para estos parámetros!
+    def __init__(self, tasa_interés, balance): 
+        # tu código aquí (recuerda, los atributos de instancia van aquí)
+        # no te preocupes por la información del usuario aquí; pronto involucraremos la clase de usuario
+    def depósito(self, amount):
+        # tu código aquí
+    def retiro(self, amount):
+        # tu código aquí
+    def mostrar_info_cuenta(self):
+        # tu código aquí
+    def generar_interés(self):
+        # tu código aquí
