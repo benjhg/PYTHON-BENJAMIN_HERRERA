d61ef4594432a822a3cda84d120855a53f915fb0
Benjhg <benjamin.herrera.garrido@alumnos.sip.cl>
2023-04-05 11:21:13 -0400

    Práctica modular

--- fundamentals/oop/Usuarios_con_Cuentas_bancarias.py
+++ fundamentals/oop/Usuarios_con_Cuentas_bancarias.py
@@ -1,62 +1,117 @@
-class CuentaBancaria:
-    def __init__(self, num_de_cuenta,name, tasa_interes=0.01,balance=0,):
-        self.tasa_interes = tasa_interes
+class BancoAccount:
+    accounts = []
+    def __init__(self,int_rate,balance):
+        self.int_rate = int_rate
         self.balance = balance
-        self.name = name
-        self.numero_cuenta = num_de_cuenta
-        
-    def numero_cuenta(self):
-        print([])       
+        BancoAccount.accounts.append(self)
 
     def deposito(self, amount):
         self.balance += amount
-        print(f"Se han depositado {amount} pesos a {self.name} Su saldo actual es {self.balance} pesos.")
         return self
 
-    def retiro(self, amount):
-        if amount > self.balance:
-            self.balance -=5
-            print("Fondos insuficientes. No se puede realizar la transacción.")
-            return self
-        else:
+    def retiro(self,amount):
+        if(self.balance - amount) >= 0:
             self.balance -= amount
-            self.balance -=5
-            print(f"Se han retirado {amount} pesos. Su saldo actual es {self.balance} pesos.")
-            return self
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
     
-    def mostrar_info_cuenta(self):
-        print(f"Tasa de interes: {self.tasa_interes}")
-        print(f"Saldo actual: {self.balance} pesos.")
+    def informacion_cuenta(self):
+        return f"{self.balance}"
+
+    def calcular_interes(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
         return self
 
-    def generar_interes(self):
-        interes_generado = self.balance * self.tasa_interes
-        self.balance += interes_generado
-        print(f"Se ha generado {interes_generado} pesos de interes. Su saldo actual es {self.balance} pesos.")
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.account = {
+            "comprobar" : BancoAccount(.02,1000),
+            "ahorros" : BancoAccount(.05,3000)
+
+        }
+        
+
+    def user_balance(self):
+        print(f"User: {self.name}, comprobar {self.account['comprobar'].display_account_info()}")
+        print(f"User: {self.name}, ahorros {self.account['ahorros'].display_account_info()}")
         return self
 
-    def mostrar_todas_las_instancias(self):
-        interes_generado = self.balance * self.tasa_interes
-        print(f"{self.name} Tu saldo actual: {self.balance} pesos.")
-        print(f"{self.name} Tu tasa de interes: {self.tasa_interes}")
-        print(f"{self.name} Tu interes generado: {interes_generado}")
-class Usuario:
-    def __init__(self, nombre, email, cuenta):
-        self.nombre = nombre
-        self.emeil = email
-        cuenta = CuentaBancaria("tasa_interes,balance")
+    def transferir_dinero(self,amount,user):
+        self.amount -= amount
+        user.amount += amount
+        self.display_user_balance()
+        user.display_user_balance()
+        return self
 
 
+benja = User("benja")
+
+benja.account['comprobar'].deposito(100)
+class BancoAccount:
+    accounts = []
+    def __init__(self,int_rate,balance):
+        self.int_rate = int_rate
+        self.balance = balance
+        BancoAccount.accounts.append(self)
+
+    def deposito(self, amount):
+        self.balance += amount
+        return self
+
+    def retiro(self,amount):
+        if(self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+    
+    def informacion_cuenta(self):
+        return f"{self.balance}"
+
+    def calcular_interes(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.account = {
+            "comprobar" : BancoAccount(.02,1000),
+            "ahorros" : BancoAccount(.05,3000)
+
+        }
+        
+
+    def user_balance(self):
+        print(f"User: {self.name}, comprobar {self.account['comprobar'].informacion_cuenta()}")
+        print(f"User: {self.name}, ahorros {self.account['ahorros'].informacion_cuenta()}")
+        return self
+
+    def transferir_dinero(self,amount,user):
+        self.amount -= amount
+        user.amount += amount
+        self.display_user_balance()
+        user.display_user_balance()
+        return self
 
 
-juan = CuentaBancaria( 1234,"Juan",)
-isa = CuentaBancaria(123,"isa",)
-pedro = CuentaBancaria(321,"Pedro",)
-'''
-pedro.deposito(1000).deposito(2000).deposito(2000).retiro(500).generar_interes()
-juan.deposito(1000).deposito(2000).retiro(100).retiro(400).retiro(300).retiro(200).generar_interes().mostrar_info_cuenta()
+benja = User("benja")
 
-juan.mostrar_todas_las_instancias()
-pedro.mostrar_todas_las_instancias()
-'''
-juan.numero_cuenta()
+benja.account['comprobar'].deposito(100)
+benja.user_balance()
